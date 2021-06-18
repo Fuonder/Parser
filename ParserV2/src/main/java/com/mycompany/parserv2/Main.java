6e78d12883f36ce856ff9c0df54d52003e1446fb
Fuonder <38396404+Fuonder@users.noreply.github.com>
2021-06-18 16:17:39 +0300

    Add files via upload

---
+++ ParserV2/src/main/java/com/mycompany/parserv2/Main.java
@@ -0,0 +1,32 @@
+package com.mycompany.parserv2;
+
+
+import com.mycompany.parserv2.Parser;
+import com.mycompany.parserv2.ElasticSearch;
+import java.io.IOException;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) throws IOException {
+    Parser a = new Parser("https://sportmail.ru/");
+    a.Request();
+    //System.out.println(a.Request());
+    //a.getPage();
+    a.stateRequest();
+    
+    ElasticSearch b = new ElasticSearch();
+    b.connect();
+    for(int i =0; i<8;i++){
+    b.insertData(a._statesList.get(i));
+    System.out.println("work in progress" + i);}
+    b.Search("title", "Бутылками");
+    b.getAggregation();
+    b.compareStates(b.getDataFromElastic(), 99);
+    //b.getDataFromElastic();
+    
+    b.disconnect();
+    }
+    
+}
